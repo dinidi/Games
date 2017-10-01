cd191772f5f58b8b890c98227c49036db1335846
dimidi <dimidi.work@gmail.com>
2017-10-01 21:51:36 +0300

    Show simple io

---
+++ game1.h
@@ -0,0 +1,18 @@
+/*
+ * game1.h
+ *
+ *  Created on: Dec 11, 2016
+ *      Author: dimidi
+ */
+
+#ifndef GAME1_H_
+#define GAME1_H_
+
+typedef struct point_struct{
+
+	float x;
+	float y;
+
+} point;
+
+#endif /* GAME1_H_ */
